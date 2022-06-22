c0d5ef9e08917318c9be4bebb34031417a24f66a
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-06-22 15:41:12 +0200

    Union Types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-//--- Lexical this
+//--------------------------------------------- Lexical this
 
 class Person {
   private _age: number;
@@ -20,7 +20,7 @@ setTimeout(person.growOld, 1000);
 growOld();
 setTimeout(() => console.log("age: ", person.age()), 2000);
 
-//--- readonly modifier
+//--------------------------------------------- readonly modifier
 
 type Point = {
   readonly x: number;
@@ -52,3 +52,45 @@ class Animal {
 const sheep = new Animal("sheep");
 console.log(sheep.name); // Allow
 sheep.name = "wolf"; // Disallow
+
+//--------------------------------------------- Union Types
+
+/**
+ * @param input a command or an array of commands
+ * @returns a single trimmed string
+ */
+function formatCommandline(input: string | string[]) {
+  let line = "";
+  if (typeof input === "string") {
+    line = input.trim();
+  } else {
+    line = input.map(x => x.trim()).join(" ");
+  }
+  return line;
+}
+
+console.log(formatCommandline("hello")); // 'hello'
+console.log(formatCommandline(["hello ", " world "])); // 'hello world'
+console.log(formatCommandline(1337)); // Error
+
+type Padding = number | string;
+
+/**
+ * Takes a string and adds `padding` to the left.
+ * If `padding` is a number, then that number of spaces is added to the left.
+ * If `padding` is a string, then `padding` is appended to the left.
+ */
+function padLeft(value: string, padding: Padding) {
+  if (typeof padding === "number") {
+    return Array(padding + 1).join(" ") + value;
+  }
+  if (typeof padding === "string") {
+    return padding + value;
+  }
+  throw new Error(`Expected number or string, got '${padding}'.`);
+}
+
+padLeft("Hello world", 4); // '    Hello world'
+padLeft("Hello world", "  "); // '  Hello world'
+padLeft("Hello world", "---"); // '---Hello world'
+padLeft("Hello world", false); // Error
