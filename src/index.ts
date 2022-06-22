57cd773a642da726044d33925974923e931f805e
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-06-22 14:30:00 +0200

    readonly modifier

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+//--- Lexical this
+
 class Person {
   private _age: number;
   constructor(_age: number) {
@@ -17,3 +19,36 @@ const growOld = person.growOld;
 setTimeout(person.growOld, 1000);
 growOld();
 setTimeout(() => console.log("age: ", person.age()), 2000);
+
+//--- readonly modifier
+
+type Point = {
+  readonly x: number;
+  y: number;
+};
+
+const point: Point = {
+  x: 0,
+  y: 0,
+};
+
+// Variable assignment
+point = { x: 1, y: 1 };
+
+// Property assignment
+point.x = 1;
+point.y = 1;
+
+// Property read
+console.log(`(${point.x}, ${point.y})`);
+
+class Animal {
+  public readonly name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const sheep = new Animal("sheep");
+console.log(sheep.name); // Allow
+sheep.name = "wolf"; // Disallow
