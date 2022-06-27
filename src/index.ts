1f8e3bd0b42c7254d5b3ea9979068616bb2b9e6e
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-06-27 17:00:02 +0200

    Null versus Undefined

--- src/index.ts
+++ src/index.ts
@@ -291,3 +291,56 @@ function getUserAge(name: string): number {
   }
   return user.age;
 }
+
+//--------------------------------------------- Null versus Undefined
+
+let notDefined: undefined = undefined;
+let notPresent: null = null;
+
+class Point3 {
+  x: number;
+  y: number;
+}
+
+const center = new Point3();
+center.x = 0;
+// center.y = 0;
+
+console.log(center.x, center.y); // 0, undefined
+
+function logVowels(value: string) {
+  console.log(value.match(/[aeiou]/gi));
+}
+
+logVowels("hello"); // ['e', 'o']
+logVowels("sky"); // null
+
+console.log(null == null); // true (of course)
+console.log(undefined == undefined); // true (of course)
+
+console.log(undefined == null); // true
+
+console.log(false == null); // false
+console.log(0 == null); // false
+console.log("" == null); // false
+
+const result = someBooleanOrNullOrUndefined();
+if (result == null) {
+  console.log("Null or Undefined", result); // null | undefined
+}
+if (result != null) {
+  console.log("Boolean", result); // true | false
+}
+
+function decorate(value: string | null | undefined) {
+  if (value == null) {
+    return value;
+  }
+  return `-- ${value.trim()} --`;
+}
+
+console.log(decorate("Hello")); // -- Hello --
+console.log(decorate("Hello World   ")); // -- Hello World --
+
+console.log(decorate(null)); // null
+console.log(decorate(undefined)); // undefined
