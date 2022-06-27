3b3d1fd8d6d21dcecfbf8ef8c4dcd4db2f17a66c
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-06-27 13:36:08 +0200

    Literal Types

--- src/index.ts
+++ src/index.ts
@@ -94,3 +94,27 @@ padLeft("Hello world", 4); // '    Hello world'
 padLeft("Hello world", "  "); // '  Hello world'
 padLeft("Hello world", "---"); // '---Hello world'
 padLeft("Hello world", false); // Error
+
+//--------------------------------------------- Literal Types
+
+type CardinalDirection = "North" | "East" | "South" | "West";
+
+let direction: CardinalDirection;
+
+direction = "North";
+direction = "n0r7h";
+direction = "South";
+
+function move(distanceMeters: number, direction: CardinalDirection) {
+  console.log(`Moving ${distanceMeters} meters towards ${direction}`);
+}
+
+type DiceValue = 1 | 2 | 3 | 4 | 5 | 6;
+
+function rollDice() {
+  return (Math.floor(Math.random() * 6) + 1) as DiceValue;
+}
+
+if (rollDice() === 7) {
+  throw new Error("Not possible!");
+}
